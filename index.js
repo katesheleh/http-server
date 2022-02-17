5ff126e710dd4113740ea881db10fba4227dd525
Katia Sheleh <kate.sheleh@gmail.com>
2022-02-17 16:49:12 +0100

    routing

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 import express from "express";
 
 const port = 8000;
-const app = express()
+const app = express();
 
-app.get('/hello', (request, response) => {
-    response.send(`Hi there!`)
+app.all("/hello", (req, res, next) => {
+  console.log(`All`);
+  next();
+});
+
+const callback = (req, res, next) => {
+  console.log("callback");
+  next();
+};
+
+// 'hel?lo' means the route 'hello' or 'helo'
+// 'hel+lo' means the route 'hello' or any count of the "l", for example "hellllllllllo"
+// 'hel*lo' (* any symbol) means the route 'hello' or any other symbol, for example "helasdfsadflo"
+// 'he(la)?lo' means the route 'helo' or 'helalo'
+// /.*c$/ any string and with any length which ends on 'c', for example 'ttdtdtsc'
+app.get("/hel?lo", callback, (req, res) => {
+  res.send(`Hi there!`);
+});
+
+app
+  .route("/user")
+  .get("/test", callback, (req, res) => {
+    res.send(`/user/test GET Hi there!`);
   })
-
-app.listen(port, () => {
-    console.log(`Server is running at http//:localhost:${port}`);
+  .post("/test", callback, (req, res) => {
+    res.send(`/user/test POST Hi there!`);
   });
 
+app.listen(port, () => {
+  console.log(`Server is running at http//:localhost:${port}`);
+});
